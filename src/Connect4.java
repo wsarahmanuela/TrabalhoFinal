ed3de887be355526684c985d0e6444f4675b6bfd
Gabrieli Bergmann <gabrielibergmann1@gmail.com>
2026-06-15 18:25:52 -0300

    inicializando

--- src/Connect4.java
+++ src/Connect4.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class Connect4 {
+
+    Scanner sc = new Scanner(System.in);
+
+    char tabuleiro[][] = new char[6][7];
+
+    public static void main(String[] args) {
+        
+    }
 
 }
